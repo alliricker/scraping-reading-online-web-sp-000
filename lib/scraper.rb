2e51e4d0eff0940a30cb04b4f0bb648101bca26e
Allison Ricker <alliricker@gmail.com>
2020-10-19 00:25:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,8 @@ doc = Nokogiri::HTML(html)
 
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css("3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
 
 courses.each do |course|
   puts course.text.strip
