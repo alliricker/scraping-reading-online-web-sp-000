5cc58de3a66b1693fef0d8e96f57fe87bdb4ba90
Allison Ricker <alliricker@gmail.com>
2020-10-19 00:31:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,15 +3,7 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-
-courses = doc.css("3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-
-
-courses.each do |course|
-  puts course.text.strip
-end
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0]
